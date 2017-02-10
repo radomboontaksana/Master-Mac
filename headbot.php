c66db91491d0f5e4158d9ee84412e6551eced074
MacShare <cmod.maod@gmail.com>
2017-02-10 17:41:34 +0700

    Create headbot.php

---
+++ headbot.php
@@ -0,0 +1,12 @@
+<?php
+
+$access_token = 'TuPeAEFb91uGz4kPxjnMeZ6QaAGrLK05ZQcO5P1zdTNr3bYhATznR5S9ef2Xr/7uNFmngRzqMu+xPUsXw3u53QOwzy2SP+RsQsMVxP6G7VxEff/5I7k0t+SrazH97wQpwxIblRSY7FLpoTkwY0l5hQdB04t89/1O/w1cDnyilFU=';
+
+$originalUrl = "https://pacific-scrubland-67443.herokuapp.com/";
+$dataPrEqUrl = "https://hidden-mountain-56713.herokuapp.com/priorityeq/";
+$dataSubUrl = "https://hidden-mountain-56713.herokuapp.com/sub/";
+$datapkUrl = "https://hidden-mountain-56713.herokuapp.com/peak/";
+$ecsURL = "https://ecs.egat.co.th/index.php/apps/gallery/ajax/image.php?file=6afd971f8d42fe9fcf578194fdf912af%2F%2FMac_Power.jpg";
+$ecsPreviewURL = "https://ecs.egat.co.th/index.php/apps/gallery/ajax/image.php?file=6afd971f8d42fe9fcf578194fdf912af%2F%2FMac_PowerPreview.JPG";
+
+?>
