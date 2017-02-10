3bbd4e2c7b7f8330ce76b29b9c5d4078cf1dd613
MacShare <cmod.maod@gmail.com>
2017-02-10 17:44:26 +0700

    Update headbot.php

--- headbot.php
+++ headbot.php
@@ -1,6 +1,6 @@
 <?php
 
-$access_token = 'TuPeAEFb91uGz4kPxjnMeZ6QaAGrLK05ZQcO5P1zdTNr3bYhATznR5S9ef2Xr/7uNFmngRzqMu+xPUsXw3u53QOwzy2SP+RsQsMVxP6G7VxEff/5I7k0t+SrazH97wQpwxIblRSY7FLpoTkwY0l5hQdB04t89/1O/w1cDnyilFU=';
+$access_token = '123';
 
 $originalUrl = "https://pacific-scrubland-67443.herokuapp.com/";
 $dataPrEqUrl = "https://hidden-mountain-56713.herokuapp.com/priorityeq/";
