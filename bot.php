d6f51e96a8ac35ec6528959de05372973e8a2732
MacShare <cmod.maod@gmail.com>
2017-02-14 19:17:33 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,6 +2,40 @@
 include 'headbot.php';
 include 'functionbot.php';
 
-echo "Master Mac say :bot.php";
+// Get POST body content
+$content = file_get_contents('php://input');
+$events = json_decode($content, true);
+
+if (!is_null($events['events'])) {
+  foreach ($events['events'] as $event) {
+    // Reply only when Follow me.
+		if (($event['type'] == 'follow') or ($event['type'] == 'join')) {
+			// Get user follow or join me
+			$touserid = $event['source']['userId'];
+			// Gen Text Reply
+			$gentext = "ขอบคุณที่ติดตามเรา";
+			// Get Replytoken
+			$replyToken = $event['replyToken'];
+			//Make a POST Request to Messaging API to reply to follower
+			$messages = t1($gentext);
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = data1($replyToken,$messages);
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+                        curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+  }
+}
 
 ?>
