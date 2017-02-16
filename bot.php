8f0bf8fdad863494a10e6734ba5bace121d5c782
MacShare <cmod.maod@gmail.com>
2017-02-16 22:06:21 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -76,6 +76,25 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 			
+			// To group Mac Share
+			$data = [
+				'to' => 'Cc7ac9ccc51f05b2a60a1abed8cf85723',
+				'messages' => [$messages]
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+			
 			// Find Group Data
 			//$url = 'https://api.line.me/v2/bot/group/'.$togroupid;
 			//$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
