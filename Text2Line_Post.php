835be8bd8bb6aa9e6fe18b373a5a4b51f0f24d8d
MacShare <cmod.maod@gmail.com>
2017-09-12 19:09:33 +0700

    Update Text2Line_Post.php

--- Text2Line_Post.php
+++ Text2Line_Post.php
@@ -23,7 +23,7 @@ if (!is_null($text)) {
 	$messages = t1($text);
 	$url = 'https://api.line.me/v2/bot/message/push';
 	$data = [
-  		'to' => 'Cc7ac9ccc51f05b2a60a1abed8cf85723',
+  		'to' => 'Cd90b89c39f5a695f6d6996c80829e269',
 		'messages' => [$messages]
 		];
 	$post = json_encode($data);
