aa92bfe90f09662e5a028701fc9fb47a4e293fa3
MacShare <cmod.maod@gmail.com>
2017-09-16 16:09:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -121,6 +121,27 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 		}
+    // Reply only when Follow me.
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			$replyToken = $event['replyToken'];
+			$text = $event['message']['text'];
+			if ($text == 'MacShare') {
+				$url = 'https://api.line.me/v2/bot/message/reply';
+				$data = temp2imgcol3($replyToken);
+				$post = json_encode($data);
+				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+				$ch = curl_init($url);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+				$result = curl_exec($ch);
+				curl_close($ch);
+				echo $result . "\r\n";				
+			}
+	
+		}
   }
 }
 
