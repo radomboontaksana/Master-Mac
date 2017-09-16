e1f1cd063b220b0e2399514b0f99384a61ab6a44
MacShare <cmod.maod@gmail.com>
2017-09-16 18:22:19 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -151,7 +151,7 @@ if (!is_null($events['events'])) {
 			$togroupid = $event['source']['groupId'];
 			$replyToken = $event['replyToken'];
 			$postbackdata = $event['postback']['data'];
-			$postbackdata = "Test Postback";
+			//$postbackdata = "Test Postback";
 			//if (($togroupid == 'Cd90b89c39f5a695f6d6996c80829e269') or ($togroupid == 'Cc7ac9ccc51f05b2a60a1abed8cf85723') or ($touserid == 'U554a18dbd36996fdb3dd95c218cf6db0')) {
 				$url = 'https://api.line.me/v2/bot/message/reply';
 				$postbackdata = t1($postbackdata);
