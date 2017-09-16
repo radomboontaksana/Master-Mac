4fdf19e125f61db3d5ec268788155b1af387081b
MacShare <cmod.maod@gmail.com>
2017-09-16 17:31:22 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -149,7 +149,7 @@ if (!is_null($events['events'])) {
 			$touserid = $event['source']['userId'];
 			$toroomid = $event['source']['roomId'];
 			$togroupid = $event['source']['groupId'];
-			$replyToken = $event['source']['groupId'];
+			$replyToken = $event['replyToken'];
 			$postbackdata = $event['postback']['data'];
 			$text = $event['message']['text'];
 			if (($text == 'MacShare') and (($togroupid == 'Cd90b89c39f5a695f6d6996c80829e269') or ($togroupid == 'Cc7ac9ccc51f05b2a60a1abed8cf85723') or ($touserid == 'U554a18dbd36996fdb3dd95c218cf6db0'))) {
