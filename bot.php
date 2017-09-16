f391acc7381a4911d56e7d7d48c77c88c0069e19
MacShare <cmod.maod@gmail.com>
2017-09-16 17:39:25 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -151,7 +151,7 @@ if (!is_null($events['events'])) {
 			$togroupid = $event['source']['groupId'];
 			$replyToken = $event['replyToken'];
 			$postbackdata = $event['postback']['data'];
-			if (($togroupid == 'Cd90b89c39f5a695f6d6996c80829e269') or ($togroupid == 'Cc7ac9ccc51f05b2a60a1abed8cf85723') or ($touserid == 'U554a18dbd36996fdb3dd95c218cf6db0')) {
+			//if (($togroupid == 'Cd90b89c39f5a695f6d6996c80829e269') or ($togroupid == 'Cc7ac9ccc51f05b2a60a1abed8cf85723') or ($touserid == 'U554a18dbd36996fdb3dd95c218cf6db0')) {
 				$url = 'https://api.line.me/v2/bot/message/reply';
 				$data = data1($replyToken,$postbackdata);
 				$post = json_encode($data);
@@ -165,7 +165,7 @@ if (!is_null($events['events'])) {
 				$result = curl_exec($ch);
 				curl_close($ch);
 				echo $result . "\r\n";				
-			}	
+			//}	
 		}
   }
 }
