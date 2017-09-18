ddaa0127bcd5a4f77b7107dc50a952341be51c06
MacShare <cmod.maod@gmail.com>
2017-09-18 19:45:52 +0700

    Update headbot.php

--- headbot.php
+++ headbot.php
@@ -2,8 +2,6 @@
 
 $access_token = 'juzbEAu+T7V0MLnZZevyeLYtSBGX0WGpS8Dcq7SPHFZvuWkT9FKm3NYrSk/ED8sP0SBK4u1c8pZyFSh5+geh2jDka8GI7pPh0DtIBGqeAgj5W9Gx3e/0+A/p66+EHMixoQVE1NH5FeNEpbdIzAe3NQdB04t89/1O/w1cDnyilFU=';
 
-$baseUrl = "https://ecs.egat.co.th/public.php?service=files&t=fd20b4335410e38c017713bd6d458deb";
-
 //https://still-inlet-83395.herokuapp.com/
 
 
