04c69d8670f40cd7512c6e6d0cf52f943425d8fb
MacShare <cmod.maod@gmail.com>
2017-09-18 19:47:42 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -120,63 +120,7 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
-		}
-    // Reply only when MacShare.
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			$touserid = $event['source']['userId'];
-			$toroomid = $event['source']['roomId'];
-			$togroupid = $event['source']['groupId'];
-			$replyToken = $event['replyToken'];
-			$text = $event['message']['text'];
-			if (($text == '00MacShare') and (($togroupid == 'Cd90b89c39f5a695f6d6996c80829e269') or ($togroupid == 'Cc7ac9ccc51f05b2a60a1abed8cf85723') or ($touserid == 'U554a18dbd36996fdb3dd95c218cf6db0'))) {
-				$url = 'https://api.line.me/v2/bot/message/reply';
-				$data = temp2imgcol3($replyToken);
-				$post = json_encode($data);
-				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-				$ch = curl_init($url);
-				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-				$result = curl_exec($ch);
-				curl_close($ch);
-				echo $result . "\r\n";				
-			}	
-		}
-    /*/ Action Postback only when MacShare.
-	  	if ($event['type'] == 'postback') {
-			$touserid = $event['source']['userId'];
-			$toroomid = $event['source']['roomId'];
-			$togroupid = $event['source']['groupId'];
-			$replyToken = $event['replyToken'];
-			$timedata = $event['timestamp'];
-			
-			//$timedata = substr($timedata,0,strlen($timedata)-3)
-
-			
-			//$timedata = $timedata. "\n" . strtotime("now") . "\n" . ((strtotime("now") * 1000) - $timedata) . "\n" . mktime ( $hour, $minute, $second, $month, $day, $year, $is_dst ). "\n" . date(DATE_RFC3339) . "\n";
-			
-			$postbackdata = $event['postback']['data'];
-			$postbackdata = $postbackdata;
-			//$postbackdata = "Test Postback";
-			if (($togroupid == 'Cd90b89c39f5a695f6d6996c80829e269') or ($togroupid == 'Cc7ac9ccc51f05b2a60a1abed8cf85723') or ($touserid == 'U554a18dbd36996fdb3dd95c218cf6db0')) {
-				$url = 'https://api.line.me/v2/bot/message/reply';
-				$postbackdata = t1($postbackdata);
-				$data = data1($replyToken,$postbackdata);
-				$post = json_encode($data);
-				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-				$ch = curl_init($url);
-				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-				$result = curl_exec($ch);
-				curl_close($ch);
-				echo $result . "\r\n";				
-			}	
-		} */
+		}    
   }
 }
 
